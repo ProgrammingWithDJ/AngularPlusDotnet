989f3a388f948eb1bdf734acef5d443b20f6fd17
Alex Wilber <AlexW@cqons.onmicrosoft.com>
2022-10-12 21:30:27 +0530

    Added new code

--- src/app/services/super-hero.service.ts
+++ src/app/services/super-hero.service.ts
@@ -1,22 +1,20 @@
 import { Injectable } from '@angular/core';
 import { SuperHero } from '../models/super-hero';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SuperHeroService {
 
-  constructor() { }
+  private url ="SuperHero";
 
-  public getSuperHeros(): SuperHero[]{
+  constructor(private http: HttpClient) { }
 
-    let hero = new SuperHero();
-    hero.id =1;
-    hero.name ="Spiderman";
-    hero.firstName="peter";
-    hero.lastName="parker";
-    hero.place="New york city";
-
-    return [hero];
+  public getSuperHeros(): Observable<SuperHero[]>{
+        
+    return this.http.get<SuperHero[]>(`${environment.apiUrl}/${this.url}`);
   }
 }
